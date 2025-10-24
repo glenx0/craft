26a6576a57967c59bdbdfd7639adad89c7d4815d
glenx0 <glendaodom3@gmail.com>
2025-10-24 09:19:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ project = random.choice(projects)
 with open("log.txt", "a", encoding="utf-8") as f:
     f.write(f"Dự án được chọn: {project}\n")
 
-print("Đã ghi vào log:", project--1--18
+print("Đã ghi vào log:", project--1--1--24
 
 --02--05--07--08--10--11--12--18--21--22--25--27--31--03--05--07--10--11--13--14--16--17--19--22--23--24--26--28--29--30--05--06--08--09--14--17--22
